5f70a278a6b27aed3cccbaac36e497ff5f72b24f
linwei <linwei2work@163.com>
2021-04-09 15:10:07 +0800

    此版实现指定半径内吸引生物，并到黑洞范围内进行销毁，但计数问题、不是以黑洞为中心获取实体ID以及不能同时实现多个黑洞等问题待解决；下一版试试羽哥推荐的那个服务端：获取区域内的entity列表——GetEntitiesInSquareArea

--- behavior_pack_5ga6n4vr/blackHoleScripts/modServer/serverSystem.py
+++ behavior_pack_5ga6n4vr/blackHoleScripts/modServer/serverSystem.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import math
 
 import mod.server.extraServerApi as serverApi
 
@@ -27,6 +28,7 @@ class BlackHoleServerSystem(ServerSystem):
         self.dict = {}
         self.count = 0
         self.flag = False
+        self.number = 0
 
     def ListenEvent(self):
         self.DefineEvent(modConfig.CreateEffectEvent)
@@ -58,7 +60,7 @@ class BlackHoleServerSystem(ServerSystem):
         evenData['z'] = args["z"]
         evenData['blockName'] = args['blockName']
         # 广播CreateEffectEvent事件通知客户端创建特效
-        # self.BroadcastToAllClient(modConfig.CreateEffectEvent, evenData)
+        self.BroadcastToAllClient(modConfig.CreateEffectEvent, evenData)
 
         # 测试
         # self.blockStart(evenData)
@@ -114,40 +116,65 @@ class BlackHoleServerSystem(ServerSystem):
 
         # 控制是否调用============================================================= 2021年4月8日21:13:53 待修改
         self.flag = True
-        dict['playerId'] = args["playerId"]
-        dict['x'] = args["x"]
-        dict['y'] = args["y"]
-        dict['z'] = args["z"]
-        print '==========================> playerId = ', dict.get('playerId')
-        print '==========================> x = ', dict.get('x')
-        print '==========================> y = ', dict.get('y')
-        print '==========================> z = ', dict.get('z')
+
+        self.dict['playerId'] = args["playerId"]
+        self.dict['x'] = args["x"]
+        self.dict['y'] = args["y"]
+        self.dict['z'] = args["z"]
 
     # 服务器tick时触发,1秒有30个tick
     def OnScriptTickServer(self):
         self.count += 1
-        if self.count % 30 == 0:
-            print '-------------------------------------------------- tick'
-            # 黑洞初始特效创建完成后，调用自定义函数，初步实现以玩家为中心，一定范围内的生物牵引功能
-            # if self.flag:
-            #     self.biologyAttract(dict.get('playerId'), dict.get('x'), dict.get('y'), dict.get('z'))
-            #     print '-------------------------------------------------- biologyAttract'
+        # 每1/15秒触发一次
+        if self.count % 2 == 0:
+            # print '-------------------------------------------------- tick', self.count
+            # 黑洞初始特效创建完成后，调用自定义函数，初步实现以玩家为范围中心，以点击方块为吸引位置中心，使用向量进行的生物牵引功能
+            if self.flag:
+                self.biologyAttract(self.dict.get('playerId'), self.dict.get('x'), self.dict.get('y'), self.dict.get('z'))
+                # print '-------------------------------------------------- biologyAttract'
 
     # 初步实现以玩家为中心，一定范围内的吸引功能
     def biologyAttract(self, player_id, x, y, z):
-        entity_ids = get_entity_api.get_pet_enemies_around(player_id, 9)
+        entity_ids = get_entity_api.get_pet_enemies_around(player_id, 10)
         # print '------------------------------ entity_ids = ', entity_ids
-        print '-----------------------------> count : ', len(entity_ids)
+        print '-----------------------------> attract_count : ', len(entity_ids)
         for id in entity_ids:
             # print '================= id = ', id
             # 获取实体位置坐标
+            print("1111111111111111111")
+            print(id)
             comp = serverApi.GetEngineCompFactory().CreatePos(id)
             entityPos = comp.GetPos()
+            print(entityPos)
             entityPosX = entityPos[0]
             entityPosY = entityPos[1]
             entityPosZ = entityPos[2]
-            set_motion(id, (float(x - entityPosX) / 10, float(y - entityPosY) / 10, float(z - entityPosZ) / 10))
-        print '========================== biologyAttract  over'
+            set_motion(id, (float(x - entityPosX) / 50, float(y - entityPosY) / 50, float(z - entityPosZ) / 50))
+
+        # 获取以玩家为中心，一定半径范围的所有生物ID（生物的具体杀死条件在下面通过生物离黑洞距离进行控制）
+        entity_ids2 = get_entity_api.get_pet_enemies_around(player_id, 1000)
+        # 下面代码实现功能：杀死进入黑洞范围内的生物
+        for entityId in entity_ids2:
+            # 获取实体位置坐标
+            comp = serverApi.GetEngineCompFactory().CreatePos(entityId)
+            entityPos = comp.GetPos()
+            entity_pos_x = entityPos[0]
+            entity_pos_y = entityPos[1]
+            entity_pos_z = entityPos[2]
+            # 获取黑洞半径范围内生物到黑洞中心的距离
+            num = (x - entity_pos_x)**2 + (y - entity_pos_y)**2 + (z - entity_pos_z)**2
+            distance = math.sqrt(num)
+            # 杀死进入黑洞范围内的生物 2021年4月9日15:02:45======================== 下面计数有些问题，先存一版，后边试试羽哥推荐的以方块范围获取实体ID
+            if distance <= 3:
+                self.number += 1
+                print '-------------------------------------------------> number = ', self.number
+                levelId = serverApi.GetLevelId()
+                comp = serverApi.GetEngineCompFactory().CreateGame(levelId)
+                comp.KillEntity(entityId)
+            if self.number == 20:
+                print '=================================> attract 20, begin change     ', self.number
+                self.number = 0
+                print '---------------------------------> after change number    ', self.number
 
     # 方块物品先搁置，先实现生物这块
     def blockStart(self, args):
